97ba6a4134047b45f8f06280a03f004ca040ac3c
Emma Fisher <emmarfisher99@gmail.com>
2018-07-03 13:58:53 -0600

    logging body

--- listFiles.js
+++ listFiles.js
@@ -34,7 +34,7 @@ function execute() {
         .then(function (response) {
             // Handle the results here (response.result has the parsed body).
             console.log('Response', response);
-            //console.log(body.id);
+            console.log(response.body.id);
 
         },
         function (err) {
